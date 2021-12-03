37352c816f87847806aae2c8036ff19400b9f24e
brunofelixf <brunofelixf@gmail.com>
2021-12-03 04:52:27 -0300

    Criando a comida

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box
 }
-
 let direction = 'right'
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
   context.fillStyle = 'lightgreen'
@@ -19,6 +22,11 @@ function criarCobra() {
     context.fillStyle = 'red'
     context.fillRect(snake[i].x, snake[i].y, box, box)
   }
+}
+
+function drawFood() {
+  context.fillStyle = 'blue'
+  context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', update)
@@ -38,6 +46,7 @@ function iniciarJogo() {
 
   criarBG()
   criarCobra()
+  drawFood()
 
   let snakeX = snake[0].x
   let snakeY = snake[0].y
